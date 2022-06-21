e4c5f72d59cc37897ab0060438c5b8443a0bf986
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-06-21 20:46:44 +0300

    troubleshooting: Strip down function

--- netlify/functions/httptest.js
+++ netlify/functions/httptest.js
@@ -1,11 +1,5 @@
 import fetch from 'node-fetch';
 
-const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts';
-
-const API_KEY = '9213432dsdajl3423209dfsdlfj43534sdfjsdfdljf322289479ddfd';
-
-let jsondata;
-
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
@@ -13,28 +7,4 @@ exports.handler = async (event, context) => {
       message: "Hello World"
     })
   }
-
-    // .then((response) => response.json())
-    // .then((data) => {
-    //   jsondata = JSON.stringify(data);
-    //   return ({
-    //     statusCode: 200,
-    //     body: JSON.stringify({ message: "Hello World" }),
-    //   });
-    // })
 };
-
-
-// exports.handler = async (event, context) => {
-//   try {
-//     const response = await fetch(API_ENDPOINT);
-//     const data = await response.json();
-//     return { statusCode: 200, body: JSON.stringify({ data }) };
-//   } catch (error) {
-//     console.log(error);
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: 'Failed fetching data' }),
-//     };
-//   }
-// };
