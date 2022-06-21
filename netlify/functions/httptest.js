7b98d2987239725407780495d783361107c00a7b
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-06-21 21:03:42 +0300

    after working fetch; now implement object response

--- netlify/functions/httptest.js
+++ netlify/functions/httptest.js
@@ -1,10 +1,17 @@
 import fetch from 'node-fetch';
 
+const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts';
+
+const API_KEY = '9213432dsdajl3423209dfsdlfj43534sdfjsdfdljf322289479ddfd';
+
+let jsondata;
+
 exports.handler = async (event, context) => {
+  let response = await fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello World"
+      data: response
     })
   }
 };
