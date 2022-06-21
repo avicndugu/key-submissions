cf0d6b75a6e574da3954b8586f7569669ca4baca
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-06-21 16:52:00 +0300

    downgrade pexels function

--- netlify/functions/pexels.js
+++ netlify/functions/pexels.js
@@ -1,6 +1,3 @@
-import { createClient } from 'pexels';
-const client = createClient(process.env.PEXELS_API_KEY);
-
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
