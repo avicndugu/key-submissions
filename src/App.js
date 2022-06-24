76728a679ce36f84dc7f6ca4d6f50267af6cea76
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-06-24 15:32:05 +0300

    Use input to change search terms

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
+const [word, setWord] = useState("");
+const [searchTerm, setSearchTerm] = useState("cup");
  
   const [data, setData] = useState([]);
-  const searchterm = 'cup'
+
   const BASIC_API_ENDPOINT = 'https://api.pexels.com/v1/search?query=';
  // Default request returns 15 items 
  // const API_ENDPOINT = BASIC_API_ENDPOINT + searchterm;
   const numberofitems = '&per_page=5';
   const orientation = '&orientation=square';
   const size = '&size=medium';
-  const API_ENDPOINT = BASIC_API_ENDPOINT + searchterm + numberofitems + orientation + size;
+  // const API_ENDPOINT = BASIC_API_ENDPOINT + searchterm + numberofitems + orientation + size;
+  const API_ENDPOINT = BASIC_API_ENDPOINT + searchTerm + numberofitems + orientation + size;
 
 
   useEffect(() => { 
@@ -33,7 +36,16 @@ function App() {
 
   return (
     <div className="App">
-      
+      <div>
+        <label htmlFor="related-images">Enter the English word you wish to translate: </label>
+        <br />
+        <input type="text" id="related-images" value={ word } onChange={(e)=> setWord(e.target.value)}/>
+        <button onClick={()=>setSearchTerm(word)}>Get images</button>
+      </div>
+
+      <div>
+
+      </div>
     </div>
   );
 }
