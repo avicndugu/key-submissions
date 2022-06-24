1b8a5ee0af7701275fc935ed8b93171f44e1d668
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-06-24 14:54:59 +0300

    Add fetch request & Trim excess markup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
  
- const [data, setData] = useState([]); 
+  const [data, setData] = useState([]);
+  const searchterm = 'cup'
+  const BASIC_API_ENDPOINT = 'https://api.pexels.com/v1/search?query=';
+ // Default request returns 15 items 
+ // const API_ENDPOINT = BASIC_API_ENDPOINT + searchterm;
+  const numberofitems = '&per_page=5';
+  const orientation = '&orientation=square';
+  const size = '&size=medium';
+  const API_ENDPOINT = BASIC_API_ENDPOINT + searchterm + numberofitems + orientation + size;
+
+
   useEffect(() => { 
     const fetchData = () => {
-      fetch('/.netlify/functions/pexelsfetch')
+      fetch(API_ENDPOINT, {
+        method: 'GET',
+        headers: {
+          "Authorization": ""
+        }
+      })
+      // fetch('/.netlify/functions/pexelsfetch')
       .then(response => response.json())
       .then((data) => {setData(data) })
       };
       fetchData();
-  },[]);
+  },[API_ENDPOINT]);
   console.log(data);
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
