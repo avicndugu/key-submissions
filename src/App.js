86258083df3219017c8a11475cdb6c4ef1f2a30b
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-08-10 00:00:20 +0300

    Load data only on button click

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { useState, useEffect } from 'react';
 
 function App() {
 const [word, setWord] = useState("");
-const [searchTerm, setSearchTerm] = useState("cup");
+const [searchTerm, setSearchTerm] = useState("grandmother");
 const [imgUrl, setImgUrl] = useState("");
 const [checked, setChecked] = useState([true, false, false]);
 const [translation, setTranslation] = useState("");
 const BASIC_API_ENDPOINT = 'https://api.pexels.com/v1/search?query=';
-
+const [isLoading, setIsLoading] = useState(false);
+const [newData, setNewData] = useState(null);
 const updateState = (id) => {
   const newState = checked.map( (item, index) => {
     if (index===id) {
@@ -52,6 +53,30 @@ const updateState = (id) => {
 
   const result = DataFetch(API_ENDPOINT);
 
+// Use click instead of useffect to load data
+  const handleClick = async (term) => {
+  const search = BASIC_API_ENDPOINT + term + numberofitems + orientation + size;
+
+    setIsLoading(true);
+    try {
+      const response = await fetch(search, {
+        method: 'GET',
+        headers: {
+          "Authorization": process.env.REACT_APP_PEXELS_API_KEY
+        }
+      })
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+      const json = await response.json();
+      setNewData(json);
+    } catch(error) {
+      console.log("error:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   // Set the url of the default image as soon as the api is fully loaded 
   useEffect(()=> {
     if(result){
@@ -64,6 +89,128 @@ const updateState = (id) => {
       }
     }
   },[result]);
+
+
+  if(isLoading || !newData){
+    return (
+      <div className="App">
+        <p>Enter the English word and its Kikuyu translation.</p>
+        <div className="container pt-1 pb-1">
+          <div className="pb-1">
+            <label htmlFor="related-images-en">English Word</label>
+            <br />
+            <br />
+            <input type="text" id="related-images-en" value={ word } onChange={(e)=> setWord(e.target.value)}/>
+          </div>
+          <div>
+            <label htmlFor="related-images-ki">Kikuyu Word</label>
+            <br />
+            <br />
+          <input type="text" id="related-images-ki" value={ translation } onChange={(e)=> setTranslation(e.target.value)}/>
+          </div>
+        </div>
+        <div className="pt-1 pb-1">
+          <p>Pictures helps everyone remember word better.</p>
+          <button className="button" onClick={()=>handleClick(word)}>Load pictures</button>
+        </div>
+        <div className="container pt-1 pb-1">
+          <div>
+            <img src="https://via.placeholder.com/280x200.png?text=loading..." alt="placeholder" width="280" height="200"/>
+          </div>
+          <div>
+            <img src="https://via.placeholder.com/280x200.png?text=loading..." alt="placeholder" width="280" height="200"/>
+          </div>
+          <div>
+            <img src="https://via.placeholder.com/280x200.png?text=loading..." alt="placeholder" width="280" height="200"/>
+          </div>
+        </div>
+        <Button imgUrl ={ imgUrl } word={ word } translation={ translation } buttontext="Submit Word Without Picture" />
+      </div>
+    )
+  } else {
+    if(newData.photos.length===0) {
+      return (
+        <div className="App">
+          <p>Enter the English word and its Kikuyu translation.</p>
+          <div className="container pt-1 pb-1">
+            <div className="pb-1">
+              <label htmlFor="related-images-en">English Word</label>
+              <br />
+              <br />
+              <input type="text" id="related-images-en" value={ word } onChange={(e)=> setWord(e.target.value)}/>
+            </div>
+            <div>
+              <label htmlFor="related-images-ki">Kikuyu Word</label>
+              <br />
+              <br />
+            <input type="text" id="related-images-ki" value={ translation } onChange={(e)=> setTranslation(e.target.value)}/>
+            </div>
+          </div>
+          <div className="pt-1 pb-1">
+            <p>Pictures helps everyone remember word better.</p>
+            <button className="button" onClick={()=>handleClick(word)}>Load pictures</button>
+          </div>
+          <div className="container pt-1 pb-1">
+            <div>
+              <img src="https://via.placeholder.com/280x200.png?text=No+pictures+found" alt="placeholder" width="280" height="200"/>
+            </div>
+            <div>
+              <img src="https://via.placeholder.com/280x200.png?text=No+pictures+found" alt="placeholder" width="280" height="200"/>
+            </div>
+            <div>
+              <img src="https://via.placeholder.com/280x200.png?text=No+pictures+found" alt="placeholder" width="280" height="200"/>
+            </div>
+          </div>
+          <Button imgUrl ={ imgUrl } word={ word } translation={ translation } buttontext="Submit Word Without Picture" />
+        </div>
+      )
+    } else {
+      return (
+        <div className="App">
+          <p>Enter the English word and its Kikuyu translation.</p>
+          <div className="container pt-1 pb-1">
+            <div className="pb-1">
+              <label htmlFor="related-images-en">English Word</label>
+              <br />
+              <br />
+              <input type="text" id="related-images-en" value={ word } onChange={(e)=> setWord(e.target.value)}/>
+            </div>
+            <div>
+              <label htmlFor="related-images-ki">Kikuyu Word</label>
+              <br />
+              <br />
+            <input type="text" id="related-images-ki" value={ translation } onChange={(e)=> setTranslation(e.target.value)}/>
+            </div>
+          </div>
+          <div className="pt-1 pb-1">
+            <p>Pictures helps everyone remember word better.</p>
+            <button className="button" onClick={()=>handleClick(word)}>Load pictures</button>
+          </div>
+          <div className="container pt-1 pb-1">
+            {
+              newData.photos.map((item, index)=>(
+                <div key={index} onClick={()=> {
+                  setImgUrl(item.src.tiny)
+                }}>
+                  <label htmlFor={index}>
+                    <img src={item.src.tiny} alt={item.alt} className={ checked[index] ? "checked" : " " } width="280" height="200" />
+                    <div>
+                      <input type="radio" name="image-chooser" id={index}  defaultChecked={checked[index]} onClick= {() => {
+                        updateState(index);
+                        setImgUrl(item.src.tiny); 
+                      }}/>
+                    </div>
+                  </label>
+                </div>
+              ))
+            }
+          </div>    
+          <Button imgUrl ={ imgUrl } word={ word } translation={ translation } buttontext="Submit Word with Pictures" />
+        </div>
+      )
+    }
+  }
+
 
   if(!result){
     return (
@@ -89,13 +236,13 @@ const updateState = (id) => {
         </div>
         <div className="container pt-1 pb-1">
           <div>
-            <img src="https://via.placeholder.com/280x200.png?text=loading..." alt="placeholder"/>
+            <img src="https://via.placeholder.com/280x200.png?text=loading..." alt="placeholder" width="280" height="200"/>
           </div>
           <div>
-            <img src="https://via.placeholder.com/280x200.png?text=loading..." alt="placeholder"/>
+            <img src="https://via.placeholder.com/280x200.png?text=loading..." alt="placeholder" width="280" height="200"/>
           </div>
           <div>
-            <img src="https://via.placeholder.com/280x200.png?text=loading..." alt="placeholder"/>
+            <img src="https://via.placeholder.com/280x200.png?text=loading..." alt="placeholder" width="280" height="200"/>
           </div>
         </div>
         <Button imgUrl ={ imgUrl } word={ word } translation={ translation } buttontext="Submit Word Without Picture" />
@@ -126,13 +273,13 @@ const updateState = (id) => {
           </div>
           <div className="container pt-1 pb-1">
             <div>
-              <img src="https://via.placeholder.com/280x200.png?text=Pictures+not+found..." alt="placeholder"/>
+              <img src="https://via.placeholder.com/280x200.png?text=Pictures+not+found..." alt="placeholder" width="280" height="200"/>
             </div>
             <div>
-              <img src="https://via.placeholder.com/280x200.png?text=Pictures+not+found..." alt="placeholder"/>
+              <img src="https://via.placeholder.com/280x200.png?text=Pictures+not+found..." alt="placeholder" width="280" height="200"/>
             </div>
             <div>
-              <img src="https://via.placeholder.com/280x200.png?text=Pictures+not+found..." alt="placeholder"/>
+              <img src="https://via.placeholder.com/280x200.png?text=Pictures+not+found..." alt="placeholder" width="280" height="200"/>
             </div>
           </div>
             <Button imgUrl ={ imgUrl } word={ word } translation={ translation } buttontext="Submit Word Without Picture" />
@@ -169,7 +316,7 @@ const updateState = (id) => {
                   setImgUrl(item.src.tiny)
                 }}>
                   <label htmlFor={index}>
-                    <img src={item.src.tiny} alt={item.alt} className={ checked[index] ? "checked" : " " }  />
+                    <img src={item.src.tiny} alt={item.alt} className={ checked[index] ? "checked" : " " } width="280" height="200" />
                     <div>
                       <input type="radio" name="image-chooser" id={index}  defaultChecked={checked[index]} onClick= {() => {
                         updateState(index);
@@ -198,7 +345,7 @@ function Button(props) {
     };
     const sent = await fetch('https://reqres.in/api/posts', requestOptions)
     const jsons = await sent.json();
-    // console.log(response.status)
+    console.log(sent.status)
   }
   return(
     <button onClick={()=>postData({ english: props.word , url: props.imgUrl , translation: props.translation })}> { props.buttontext } </button>
